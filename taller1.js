4b4227fc25840e266772d986dc56f90b94772bc2
Laureano55 <110035786+Laureano55@users.noreply.github.com>
2025-08-08 18:17:20 -0500

    test upload file

---
+++ taller1.js
@@ -0,0 +1,13 @@
+// la vida
+let variable1 = 132 //declaración de variable normal
+console.log(variable1)
+console.log("hola")
+
+
+const gd = 1 //no se puede cambiar el valor 
+
+let nombre = "lau"
+let apellido = "lafaurie"
+
+console.log(nombre + " " + apellido)
+
